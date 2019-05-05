c57f9c908fe7a2f37433e88020f647edc7974996
luqmanMohammed <mohammed.luqman@pearson.com>
2019-05-05 10:18:38 +0530

    added total compute resource calculation

--- application_cost.py
+++ application_cost.py
@@ -1,1 +1,22 @@
 #namespace level costing
+import kubernetes
+from kubernetes import client,config
+from pprint import pprint
+
+config.load_kube_config()
+v1 = client.CoreV1Api()
+
+total_cluster_cost = 500
+
+#Calculate minion total compute resources
+total_cpu = 0
+total_memory = 0
+try:
+    #nodes = v1.list_node(label_selector="role=minion")
+    api_responce = v1.list_node(pretty=True,field_selector="metadata.name=minikube")
+    for node in api_responce.items:
+        total_cpu += int(node.status.capacity["cpu"])
+        memory_str = node.status.capacity["memory"]
+        total_memory += int(memory_str[0:len(memory_str) - 2])
+except Exception as e:
+    print e
