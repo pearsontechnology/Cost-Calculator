4d2befd7d5699fe62810768e6b39af3f12559ee0
luqmanMohammed <mohammed.luqman@pearson.com>
2019-05-06 19:11:44 +0530

    changed main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import schedule
 import time
-
+from datetime import datetime
 from application_cost import main_procedure
-
-schedule.every().hour.at("00:00").do(main_procedure) ### RUNS EVERY HOUR At @HOUR:00
-main_procedure()
 while True:
-    schedule.run_pending()
-    time.sleep(1)
+    main_procedure()
+    time.sleep(60 * 60)
