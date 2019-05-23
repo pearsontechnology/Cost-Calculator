ab7c603a58b9e8cdf04485dd594ee09afa22205e
luqmanMohammed <mohammed.luqman@pearson.com>
2019-05-23 15:10:05 +0530

    added costing for ec2 based crd

--- crd_costs/crd_cost.py
+++ crd_costs/crd_cost.py
@@ -7,7 +7,8 @@ import os
 config.load_kube_config()
 v1 = client.CoreV1Api()
 
-def calc_crd_by_namespace(environment,environment_type,namespace):
+#tested with CB and Mongo
+def calc_ec2_based_crd_cost(environment,environment_type,namespace):
 
     api_instance = kubernetes.client.CustomObjectsApi(kubernetes.client.ApiClient())
     group = 'prsn.io' 
@@ -15,20 +16,23 @@ def calc_crd_by_namespace(environment,environment_type,namespace):
     
     #role   :   plural in CRD
     considered_crd = {
-        "cb" : "cbs"
+        "cb" : "cbs",
+        "mg":"mongos"
     }
-
     for role,crd_plural in considered_crd.iteritems():
         api_response = api_instance.list_namespaced_custom_object(group, version,namespace,crd_plural)
         responce_items = api_response.__getitem__("items")
         if(len(responce_items) != 0):
+             calculated_names = []
              for item in responce_items:
                  calc_name = item.__getitem__("metadata").__getitem__("name")
                  calc_name += "-" + namespace
                  calc_name += "-" + role
                  calc_name += "-" + environment
                  calc_name += "-" + environment_type
+                 calculated_names.append(item)
                  print(calc_name)
+                
         else:
             print("No resources. Skipping")
 
@@ -41,5 +45,5 @@ ENVIRONMENT_TYPE = os.environ['ENVIRONMENT_TYPE'] if "ENVIRONMENT_TYPE" in os.en
 
 for namespace in namespaces.items:
     namespace_name = namespace.metadata.name
-    calc_crd_by_namespace(ENVIRONMENT,ENVIRONMENT_TYPE, namespace_name)
+    calc_ec2_based_crd_cost(ENVIRONMENT,ENVIRONMENT_TYPE, namespace_name)
     
